b7011c88e2f42e490c2e2a48febcb1154ce49301
melissadiep94 <83740864+melissadiep94@users.noreply.github.com>
2021-06-22 23:36:01 -0400

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,48 @@
+import os
 
+# Module for reading CSV files
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+#define the lists
+fields = []
+rows = []
+idList = []
+votes = 0
+winner_votes = 0
+candidates = []
+candidate_votes = []
+results = []
+
+# Method 2: Improved Reading using CSV module
+
+with open(csvpath, newline= '') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #print(csvreader)
+
+    # Read the header row first (skip this step if there is no header)
+    fields = next(csvreader)
+    #print(f"Field Names: {fields}")
+
+    #calculating # of rows and total revenue
+    total_votes = 0
+    for row in csvreader:
+    
+        #sums votes
+        total_votes+= 1
+        idList.append(row[0])
+        candidates.append(row[2])
+      #PRINT
+
+    print("Election Results")
+    print("---------------------")
+    print("Total Votes: "  +str(total_votes))
+    print("---------------------")
+
+  #gets harder from here
+#--------------------------------------------------------------------------------------------------------------     
+    
